7fe13b8d9584268f544c00e6c3e309f2857e4d4e
nayeon <20192279@sungshin.ac.kr>
2024-06-27 11:40:23 +0900

    edit

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,11 +1,14 @@
-const headerToggle=document.getElementById("headerToggle");
-const headerNav=document.querySelector(".header_nav")
+export function menu() {
+    const headerToggle = document.getElementById("headerToggle");
+    const headerNav = document.querySelector(".header__nav");
 
-if(headerToggle){
-    headerToggle.addEventListener("click",()=>{
-        headerNav.classList.toggle("show");
-        headerToggle.getAttribute("area-expended")==="true"
-        ?headerToggle.setAttribute("area-expended","false")
-        :headerToggle.setAttribute("area-expended",true);
-    });
+    if(headerToggle){
+        headerToggle.addEventListener("click", () => {
+            headerNav.classList.toggle("show");
+    
+            headerToggle.getAttribute("aria-expanded") === "true" 
+            ? headerToggle.setAttribute("aria-expanded", "false") 
+            : headerToggle.setAttribute("aria-expanded", "true");
+        });
+    }
 }
