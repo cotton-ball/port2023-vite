75987571265bacfc96176ffedf5f2cb3ed177025
nayeon <20192279@sungshin.ac.kr>
2024-06-27 10:16:33 +0900

    header

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,11 @@
-export function menu() {
-    console.log(menu)
+const headerToggle=document.getElementById("headerToggle");
+const headerNav=document.querySelector(".header_nav")
+
+if(headerToggle){
+    headerToggle.addEventListener("click",()=>{
+        headerNav.classList.toggle("show");
+        headerToggle.getAttribute("area-expended")==="true"
+        ?headerToggle.setAttribute("area-expended","false")
+        :headerToggle.setAttribute("area-expended",true);
+    });
 }
